f1c07d88397293afdd2f7b07f66ed600276412a4
WaghmareDeepali <92779683+WaghmareDeepali@users.noreply.github.com>
2024-04-30 14:30:03 +0530

    API

---
+++ watch_list/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from watch_list.views import movie_list,movie_deatils
+urlpatterns = [
+    path('list/', movie_list,name='movie-list'),
+    path('<int:pk>/', movie_deatils,name='movie-deatils'),
+
+]
---
+++ watch_list/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from watch_list.models import Movie
+from django.http import JsonResponse
+
+# Create your views here.
+def movie_list(request):
+    movies=Movie.objects.all()
+    data={'Movies':list(movies.values())}
+
+    return JsonResponse(data)
+
+
+def movie_deatils(request,pk):
+    movie=Movie.objects.get(pk=pk)
+    data={
+        'name':movie.name,
+        'description':movie.description,
+        'active':movie.active
+    }
+    print(movie)
+    return JsonResponse(data)
